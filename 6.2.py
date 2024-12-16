1739915588dc0b0d1fb03f850b01f4b46fd6790b
Dozy-y <igorkazanov1706@gmail.com>
2024-12-16 11:50:30 +0300

    Update 6.2.py

--- 6.2.py
+++ 6.2.py
@@ -2,15 +2,11 @@ class Vehicle:
     def __init__(self,make,model):
         self.make = make
         self.model = model
-    def get_info(self):
-        return self.make, self.model
 class Car(Vehicle):
     def __init__(self, make, model, fuel_type):
         super().__init__(make, model)
         self.fuel_type = fuel_type
-        def get_info():
-            return self.make, self.model, self.fuel_type
-Vehicle1 = Vehicle('Toyota','Camry')
-print(Vehicle1.get_info())
+    def get_info(self):
+        return self.make, self.model, self.fuel_type
 Car1=Car('Toyota', 'Supra', '95')
 print(Car1.get_info())
